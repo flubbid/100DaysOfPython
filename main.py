75b3574b20adcb8fb8e97df485f8e1138a69875c
flubbid <brendanwilson1@gmail.com>
2021-12-11 15:10:33 -0500

    adds Exercise 3

---
+++ main.py
@@ -0,0 +1,34 @@
+# 🚨 Don't change the code below 👇
+# height = float(input("enter your height in m: "))
+# weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+#BMI 2.0 
+
+# bmi = round(weight / height**2)
+# bmis = str(bmi)
+
+# if bmi < 18.5:
+#     print("Your BMI is {bmis}, you are underweight.")
+# elif bmi > 18.5 and bmi < 25:
+#     print(f"Your BMI is {bmis}, you have a normal weight.")
+# elif bmi > 25 and bmi < 30:
+#     print(f"Your BMI is {bmis}, you are slightly overweight.")
+# elif bmi > 30 and bmi < 35:
+#     print(f"Your BMI is {bmis}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmis}, you are clinically obese.")
+
+# Exercise 3 - Leap Year
+# year = int(input("Which year do you want to check? "))
+# if year % 4 == 0 and year % 400:
+#     print("Leap year.")
+# elif year % 4 == 0 and year % 100 != 0:
+#     print("Not leap year.")
+# elif year % 4 == 0:
+#     print("Leap year.")
+# else:
+#     print("Not leap year.")
+
