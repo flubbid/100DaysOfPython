b14e499284891ba75163e39fb3df3b6eb836ef0b
flubbid <brendanwilson1@gmail.com>
2021-12-13 23:22:49 -0500

    Adds Day 5 Password Generator exercise

--- main.py
+++ main.py
@@ -248,12 +248,47 @@
 
 # print(total)
 
-for number in range(1, 101):
-    output = ""
-    if(number % 3 == 0):
-        output += "Fizz"
-    if(number % 5 == 0):
-        output += "Buzz"
-    if(output == ""):
-        output = number
-    print(output)
+# for number in range(1, 101):
+#     output = ""
+#     if(number % 3 == 0):
+#         output += "Fizz"
+#     if(number % 5 == 0):
+#         output += "Buzz"
+#     if(output == ""):
+#         output = number
+#     print(output)
+
+
+# 5 Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+output = ""
+
+
+for letter in range(0, nr_letters + 1):
+    output += letters[random.randint(0, len(letters) - 1)]
+for symbol in range(0, nr_symbols + 1):
+    output += symbols[random.randint(0, len(symbols) - 1)]
+for number in range(0, nr_numbers + 1):
+    output += numbers[random.randint(0, len(numbers) - 1)]
+
+numList = []
+
+for char in range(0, len(output) - 1):
+    numList.append(output[char])
+    
+randomizedOutput = ""
+random.shuffle(numList)
+for item in numList:
+    randomizedOutput += item
+        
+print("Random Output:  " + randomizedOutput)
+  
